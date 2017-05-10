828ff4ec35b536ca0d748e2ea89cbf74c8971947
christinegillis <chrisa8300@gmail.com>
2017-05-10 14:25:55 -0400

    Tier 1 complete

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,21 +1,39 @@
 var express = require("express");
 var router = express.Router();
-
+var db = require("../models")
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
- 
+  db.Burger.findAll({}).then(function(data){
+    //dynamic data for handlebars
+    var hbsObject = {
+      burgers: data
+    };
+    res.render("index", hbsObject);
+  });
 });
 
 router.post("/", function(req, res) {
-	
+	db.Burger.create({
+    burger_name: req.body.burger_name,
+  }).then(function(data){
+    res.redirect("/");
+  });
 });
 
 router.put("/:id", function(req, res) {
- 
+ // var condition = parseInt(req.params.id);
+  var condition = {
+    devoured: true
+ };
+
+  db.Burger.update(condition, {
+    where: {
+      id: parseInt(req.params.id)
+    }
+  }).then(function(data){
+    res.redirect("/");
+  });
 });
-
-
-
 
 module.exports = router;
