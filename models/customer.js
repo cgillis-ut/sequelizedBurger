22df6dd086ca6d065c24742fe9019c695e426e3c
christinegillis <chrisa8300@gmail.com>
2017-05-12 20:51:48 -0400

    adds customer

---
+++ models/customer.js
@@ -0,0 +1,18 @@
+module.exports = function(sequelize, DataTypes){
+	var Customer = sequelize.define("Customer", {
+    name: {
+    	type: DataTypes.STRING,
+    	allowNull: false,
+		},
+   	{
+      classMethods: {
+        associate: function(models) {
+          Customer.hasMany(models.Burger, {
+            onDelete: "cascade"
+          });
+        }
+      }
+    }
+});
+  return Customer;
+};
